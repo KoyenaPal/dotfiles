3fd41ec03e7339a2d68bede5053cce083871b067
Koyena Pal <34932871+KoyenaPal@users.noreply.github.com>
2025-07-07 11:23:24 -0400

    Update current.sh

--- current.sh
+++ current.sh
@@ -3,9 +3,10 @@ source dotfiles/runpod.sh
 # Repos I'm using at the moment
 git clone git@github.com:KoyenaPal/MedCalc-Bench.git
 
-cd /root/MedCalc-Bench && git checkout dev && uv pip install -e .
+cd /root/MedCalc-Bench && uv pip install -e .
 # cd /root/steering-finetuning && git checkout cleanup && uv pip install -r requirements.txt
 
 uv pip install wandb
+uv pip install nnsight
 
 wandb login $RUNPOD_SECRET_WANDB
